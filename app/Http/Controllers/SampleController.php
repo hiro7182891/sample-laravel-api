4e49b6b0a3d3f91153c0149dbeac6d1b285c3e61
hiro <h.7685231@gmail.com>
2022-10-24 15:42:15 +0900

    first commit

---
+++ app/Http/Controllers/SampleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class SampleController extends Controller
+{
+    //
+    public function execute()
+    {
+        return "response";
+    }
+
+    
+
+}
